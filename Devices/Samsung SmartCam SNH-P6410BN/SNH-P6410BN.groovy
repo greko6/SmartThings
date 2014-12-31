c0b307e91b78c1aa6dcc8c8f827a87b5702476ee
KowszewiczGrzegorz <gregk@gregk-mbp1.local>
2014-12-30 20:58:08 -0800

    cleaned up the code a bit

--- Devices/Samsung SmartCam SNH-P6410BN/SNH-P6410BN.groovy
+++ Devices/Samsung SmartCam SNH-P6410BN/SNH-P6410BN.groovy
@@ -1,5 +1,33 @@
 /**
- *  SmartCam SNH-P6410BN integration
+ *  SmartThings HUB + Samsung SmartCam SNH-P6410BN integration with www digest authentication.
+ *
+ *  Description: Apparently HUB API is not allowing for two physicalgraph.device.HubAction in a row in a short period of time.
+ *               Digest web authentication needs that to get the token. As the workaround, there is a dedicated button to do the
+ *               auth first, then after successful auth, button will change color to green and you will be able to take pictures.
+ *               To pass data between buttons, I'm using special "state" variable. More details here: google smartapp-developers-guide/state.html.
+ *
+ *  Setup by adding a new device:
+ *          1. Go to: https://graph.api.smartthings.com/ide/devices and click on "New SmartDevice"
+ *          2. Add name (example: SmartCam), author and namespace (that's it). Click on "Create".
+ *          3. Edit it and paste this file.
+ *          4. Go to: https://graph.api.smartthings.com/device/list
+ *          5. Click on "New Device". Add Name, Device Network Id (put anything there), Type: SmartCam (same as you added in step 2 above),
+ *             Version: Published, Location: your hub name
+ *          6. Immediately after adding the device, click on the "Edit" link next to the "Preferences" entry in the table.
+ *          7. Populate settings:
+ *             a) Camera IP Address (192.168, 172.1x or 10.x.x.x works fine too)
+ *             b) Camera Port (usually 80, but can be anything you like)
+ *             c) Camera Path to Image, when it comes to SNH-P6410BN, use: "/cgi-bin/video.cgi?msubmenu=jpg&resolution=2".
+ *                Note: with SNH-P6410BN you can modify resolution from 1 to 10.
+ *             d) Does Camera use a Post or Get, normally Get? GET
+ *             e) Camera User, for SNH-P6410BN it's admin
+ *             f) Camera Password: anything you've set during your camera setup
+ *           8. Go to your mobile app and add a new widget.
+ *           9. Click on "Auth" button and see if you can sucessfully authenticate with your local camera. If not, triple check your settings.
+ *              Note: you can look at your logs in the real time if you're curious what's happening: https://graph.api.smartthings.com/ide/logs
+ *
+ *  Date: 2014-12-30
+ *  Version: 1.0 (tested and working)
  *
  *  Copyright 2014 Grzegorz Kowszewicz
  *
@@ -12,12 +40,10 @@
  *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
  *  for the specific language governing permissions and limitations under the License.
  *
- *  Date: 2014-12-30
- *  Version: 1.0 Stable (and tested)
- *
  */
+
 metadata {
-	definition (name: "SmartCam", namespace: "SmartCam/mojacamera", author: "Grzegorz Kowszewicz") {
+	definition (name: "SmartCam", namespace: "KowszewiczGrzegorz/SmartThings", author: "Grzegorz Kowszewicz") {
 		capability "Actuator"
 		capability "Sensor"
 		capability "Image Capture"
@@ -30,7 +56,6 @@ metadata {
     input("CameraIP", "string", title:"Camera IP Address", description: "Please enter your camera's IP Address", required: true, displayDuringSetup: true)
     input("CameraPort", "string", title:"Camera Port", description: "Please enter your camera's Port", defaultValue: 80 , required: true, displayDuringSetup: true)
     input("CameraPath", "string", title:"Camera Path to Image", description: "Please enter the path to the image (default: /cgi-bin/video.cgi?msubmenu=jpg&resolution=2)", defaultValue: "/cgi-bin/video.cgi?msubmenu=jpg&resolution=2", required: true, displayDuringSetup: true)
-    input("CameraAuth", "bool", title:"Does Camera require User Auth?", description: "Please choose if the camera requires authentication (only basic is supported)", defaultValue: true, displayDuringSetup: true)
     input("CameraPostGet", "string", title:"Does Camera use a Post or Get, normally Get?", description: "Please choose if the camera uses a POST or a GET command to retreive the image", defaultValue: "GET", displayDuringSetup: true)
     input("CameraUser", "string", title:"Camera User", description: "Please enter your camera's username (default: admin)", defaultValue: "admin", required: false, displayDuringSetup: true)
     input("CameraPassword", "string", title:"Camera Password", description: "Please enter your camera's password", required: false, displayDuringSetup: true)
@@ -62,122 +87,147 @@ metadata {
 	}
 }
 
+// method to set digest token
 def setAuthToken() {
- log.debug "setAuth"
- state.auth = "empty"
- take()
+	log.debug "setAuth"
+	state.auth = "empty"
+	take()
 }
 
+// method to set remove token (a.k.a. logout)
 def removeAuthToken() {
- log.debug "removeAuth"
- sendEvent(name: "authenticate", value: "Auth")
- state.auth = "empty"
+	log.debug "removeAuth"
+	sendEvent(name: "authenticate",
+	value: "Auth")
+	state.auth = "empty"
 }
 
-def parse(String output) {
-    log.debug "Parsing output: '${output}'"
-    def headers = ""
-    def parsedHeaders = ""
-    def map = stringToMap(output)
+// method called after touching the take button
+def take() {
+	def porthex = convertPortToHex(CameraPort)
+	def hosthex = convertIPtoHex(CameraIP)
+	def path = CameraPath.trim()
+	def request = ""
 
-    if(map.headers) {
-      headers = new String(map.headers.decodeBase64())
-      parsedHeaders = parseHttpHeaders(headers)
-    
-      if (parsedHeaders.auth) {
-        state.auth = parsedHeaders.auth
-        log.debug "Got 401, send request again (click on 'take' one more time): " + state.auth
-        sendEvent(name: "authenticate", value: "DeAuth")
-        return result
-      }
-    }
+	// set a proper network Id of the device
+	device.deviceNetworkId = "$hosthex:$porthex"
 
-    if (map.body != null) {
-      def bodyString = new String(map.body.decodeBase64())
-      log.debug bodyString
-    }
+	log.debug "The device id configured is: $device.deviceNetworkId"
+	log.debug "state: " + state
 
-    if (map.bucket && map.key) {
-        log.debug "I've got the pic. Uploading to S3"
-    	putImageInS3(map)
-    }
-    
-    return result
-}
-
-private parseHttpHeaders(String headers) {
-    def lines = headers.readLines()
-    def status = lines[0].split()
-    
-    def result = [
-        protocol:   status[0],
-        status:     status[1].toInteger(),
-        reason:     status[2]
-    ]
-
-    if (result.status == 401) {
-        result.auth = stringToMap(lines[1].replaceAll("WWW-Authenticate: Digest ", "").replaceAll("=",":").replaceAll("\"", ""))
-        log.debug "It's ok. Press take again" + result.auth
-    }
-    
-    if (result.status == 200) {
-       log.debug "Authentication successful! :" + result
-    }
-    
-    return result
-}
-
-def putImageInS3(map) {
-	def s3ObjectContent
+	if (!state.auth || state.auth == "empty") {
+		// empty request to get nonce token
+		request = """GET ${path} HTTP/1.1\r\nAccept: */*\r\nHost: ${getHostAddress()}\r\n\r\n"""
+	} else {
+		// got nonce token, parsing headers and calculating digest header
+		def auth_headers = calcDigestAuth(state.auth)
+		request = """GET ${path} HTTP/1.1\r\nAccept: */*\r\nHost: ${getHostAddress()}\r\nAuthorization: ${auth_headers}\r\n\r\n"""
+	}
 
 	try {
-		def imageBytes = getS3Object(map.bucket, map.key + ".jpg")
-
-		if(imageBytes)
-		{
-			s3ObjectContent = imageBytes.getObjectContent()
-			def bytes = new ByteArrayInputStream(s3ObjectContent.bytes)
-			storeImage(getPictureName(), bytes)
+		def hubAction = new physicalgraph.device.HubAction(request, physicalgraph.device.Protocol.LAN, "${device.deviceNetworkId}")
+		if (state.auth && state.auth != "empty") {
+			// upload image/jpg output to S3 
+			hubAction.options = [outputMsgToS3: true]
 		}
-	}
-	catch(Exception e) {
-		log.error e
-	}
-	finally {
-		if (s3ObjectContent) { s3ObjectContent.close() }
+		log.debug hubAction.getProperties()
+		return hubAction
+	} catch (Exception e) {
+		log.debug "Hit Exception $e on $hubAction"
 	}
 }
 
+// method to parse output from the camera
+def parse(String output) {
+	log.debug "Parsing output: '${output}'"
+	def headers = ""
+	def parsedHeaders = ""
+	def map = stringToMap(output)
 
-def take() {
-    def host = CameraIP
-    def porthex = convertPortToHex(CameraPort)    
-    def hosthex = convertIPtoHex(CameraIP)
-    def path = CameraPath.trim()
-    def request = ""
-    device.deviceNetworkId = "$hosthex:$porthex"
-    
-    log.debug "The device id configured is: $device.deviceNetworkId" 
-    log.debug "state: " + state
+	if (map.headers) {
+		headers = new String(map.headers.decodeBase64())
+		parsedHeaders = parseHttpHeaders(headers)
 
-    if (!state.auth || state.auth == "empty") {
-      request = """GET ${path} HTTP/1.1\r\nAccept: */*\r\nHost: ${getHostAddress()}\r\n\r\n"""
-    } else {
-      def auth_headers = calcDigestAuth(state.auth)
-      request = """GET ${path} HTTP/1.1\r\nAccept: */*\r\nHost: ${getHostAddress()}\r\nAuthorization: ${auth_headers}\r\n\r\n"""
-    }
+		if (parsedHeaders.auth) {
+			// set required tokens in the special state variable (see description above)
+			state.auth = parsedHeaders.auth
+			log.debug "Got 401, send request again (click on 'take' one more time): " + state.auth
+			sendEvent(name: "authenticate", value: "DeAuth")
+			return result
+		}
+	}
 
-    try {
-      def hubAction = new physicalgraph.device.HubAction(request, physicalgraph.device.Protocol.LAN, "${device.deviceNetworkId}")
-      if (state.auth && state.auth != "empty") {
-        hubAction.options = [outputMsgToS3:true]
-      }
-      log.debug hubAction.getProperties()
-      return hubAction
-    }
-    catch (Exception e) {
-      log.debug "Hit Exception $e on $hubAction"
-    }
+	if (map.body != null) {
+		def bodyString = new String(map.body.decodeBase64())
+		log.debug bodyString
+	}
+
+	if (map.bucket && map.key) {
+		log.debug "Uploading the picture to amazon S3"
+		putImageInS3(map)
+	}
+
+	return result
+}
+
+
+// parse headers that are returned from the camera
+private parseHttpHeaders(String headers) {
+	def lines = headers.readLines()
+	def status = lines[0].split()
+
+	def result = [
+	  protocol: status[0],
+	  status: status[1].toInteger(),
+	  reason: status[2]
+	]
+
+	if (result.status == 401) {
+		result.auth = stringToMap(lines[1].replaceAll("WWW-Authenticate: Digest ", "").replaceAll("=", ":").replaceAll("\"", ""))
+		log.debug "It's ok. Press take again" + result.auth
+	}
+
+	if (result.status == 200) {
+		log.debug "Authentication successful! :" + result
+	}
+
+	return result
+}
+
+
+// calculate digest token, more details: http://en.wikipedia.org/wiki/Digest_access_authentication#Overview
+private String calcDigestAuth(headers) {
+	def HA1 = new String("${CameraUser}:" + headers.realm.trim() + ":${CameraPassword}").trim().encodeAsMD5()
+	def HA2 = new String("${CameraPostGet}:${CameraPath}").trim().encodeAsMD5()
+
+	// increase nc every request by one
+	if (!state.nc) {
+		state.nc = 1
+	} else {
+		state.nc = state.nc + 1
+	}
+
+	def random = java.util.UUID.randomUUID().toString().replaceAll('-', '').substring(0, 8)
+	def cnonce = random
+	def response = new String("${HA1}:" + headers.nonce.trim() + ":" + state.nc + ":" + cnonce + ":" + "auth" + ":${HA2}")
+	def response_enc = response.encodeAsMD5()
+
+	// log.debug "HA1: " + HA1 + " ===== org:" + "${CameraUser}:" + headers.realm.trim() + ":${CameraPassword}"
+	// log.debug "HA2: " + HA2 + " ===== org:" + "${CameraPostGet}:${CameraPath}"
+	// log.debug "Response: " + response_enc + " =====   org:" + response
+	
+	def eol = " "
+        
+    return 'Digest username="' + CameraUser.trim() + '",' + eol +
+           'realm="' + headers.realm.trim() + '",' + eol +
+           'qop="' + headers.qop.trim() + '",' + eol +
+           'algorithm="MD5",' + eol +
+           'uri="'+ CameraPath.trim() + '",' +  eol +
+           'nonce="' + headers.nonce.trim() + '",' + eol +
+           'cnonce="' + cnonce.trim() + '",'.trim() + eol +
+           'opaque="",' + eol +
+           'nc=' + state.nc + ',' + eol +
+           'response="' + response_enc.trim() + '"'
 }
 
 private getPictureName() {
@@ -198,12 +248,12 @@ private String convertPortToHex(port) {
 }
 
 private Integer convertHexToInt(hex) {
-	Integer.parseInt(hex,16)
+	Integer.parseInt(hex, 16)
 }
 
 
 private String convertHexToIP(hex) {
-	[convertHexToInt(hex[0..1]),convertHexToInt(hex[2..3]),convertHexToInt(hex[4..5]),convertHexToInt(hex[6..7])].join(".")
+	[convertHexToInt(hex[0..1]), convertHexToInt(hex[2..3]), convertHexToInt(hex[4..5]), convertHexToInt(hex[6..7])].join(".")
 }
 
 private getHostAddress() {
@@ -213,46 +263,33 @@ private getHostAddress() {
 	return ip + ":" + port
 }
 
-
 private hashMD5(String somethingToHash) {
 	java.security.MessageDigest.getInstance("MD5").digest(somethingToHash.getBytes("UTF-8")).encodeHex().toString()
 }
 
-private String calcDigestAuth(headers) {
-	def HA1 = new String("${CameraUser}:" + headers.realm.trim() + ":${CameraPassword}").trim().encodeAsMD5()
-	def HA2 = new String("${CameraPostGet}:${CameraPath}").trim().encodeAsMD5()
+// store image on S3. Hint: if you use your bucket and key maybe you can upload it to your cloud? Never tested, but possible it will work.
+def putImageInS3(map) {
+	def s3ObjectContent
 
-    if(!state.nc) {
-      state.nc = 1
-    } else {
-      state.nc = state.nc + 1
-    }
+	try {
+		def imageBytes = getS3Object(map.bucket, map.key + ".jpg")
 
-    def random = java.util.UUID.randomUUID().toString().replaceAll('-', '').substring(0, 8)
-	def cnonce = random
-    def response = new String("${HA1}:" + headers.nonce.trim() + ":" + state.nc + ":" + cnonce + ":" + "auth" + ":${HA2}")
-    def response_enc = response.encodeAsMD5()
-
-//    log.debug "HA1: " + HA1 + " ===== org:" + "${CameraUser}:" + headers.realm.trim() + ":${CameraPassword}"
-//    log.debug "HA2: " + HA2 + " ===== org:" + "${CameraPostGet}:${CameraPath}"
-//    log.debug "Response: " + response_enc + " =====   org:" + response
-
-    def eol = " "
-        
-	return 'Digest username="' + CameraUser.trim() + '",' + eol +
-           'realm="' + headers.realm.trim() + '",' + eol +
-           'qop="' + headers.qop.trim() + '",' + eol +
-           'algorithm="MD5",' + eol +
-           'uri="'+ CameraPath.trim() + '",' +  eol +
-           'nonce="' + headers.nonce.trim() + '",' + eol +
-           'cnonce="' + cnonce.trim() + '",'.trim() + eol +
-           'opaque="",' + eol +
-           'nc=' + state.nc + ',' + eol +
-           'response="' + response_enc.trim() + '"'
+		if (imageBytes) {
+			s3ObjectContent = imageBytes.getObjectContent()
+			def bytes = new ByteArrayInputStream(s3ObjectContent.bytes)
+			storeImage(getPictureName(), bytes)
+		}
+	} catch (Exception e) {
+		log.error e
+	} finally {
+		if (s3ObjectContent) {
+			s3ObjectContent.close()
+		}
+	}
 }
 
 private def delayHubAction(ms) {
-    return new physicalgraph.device.HubAction("delay ${ms}")
+	return new physicalgraph.device.HubAction("delay ${ms}")
 }
 
 private getCallBackAddress() {
